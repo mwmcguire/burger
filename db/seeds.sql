67768cfb9e6110e4f6e368a6d05e59d17f128397
“Michael_McGuire“ <49034356+mwmcguire@users.noreply.github.com>
2019-07-27 23:43:10 -0400

    main app complete

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Holy Chuck");
+INSERT INTO burgers (burger_name) VALUES ("The County General");
+INSERT INTO burgers (burger_name) VALUES ("Golden Star");
---
+++ models/burger.js
@@ -0,0 +1,29 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+  create: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+  update: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
